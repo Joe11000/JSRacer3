90a748c8f34df38c3d2657497683c74346e05e08
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-13 15:16:14 -0500

    First Commit off of JSRacer2

---
+++ app/controllers/index.rb
@@ -0,0 +1,35 @@
+get '/' do
+  # Look in app/views/index.erb
+  erb :index
+end
+
+
+post '/victory' do
+  @winner = Player.find_by_name(params[:winner])
+  @loser = Player.find_by_name(params[:loser])
+  @game = Game.last.update_attributes(race_time: params[:raceTime], winner: @winner.id, loser: @loser.id)
+  @ranking = Game.where(winner: @winner.id).order("race_time").limit(10)
+  erb :victory, layout: false
+end
+
+post '/racer' do
+  @player1 = Player.find_or_create_by_name(params[:post][:name1])
+  @player2 = Player.find_or_create_by_name(params[:post][:name2])
+  @game = Game.create
+  @round1 = Round.create(player_id: @player1.id, game_id: @game.id) 
+  @round2 = Round.create(player_id: @player2.id, game_id: @game.id)
+  erb :racer
+end
+
+# saving 
+post '/saving_race' do
+  
+  @winner = Player.find_by_name(params[:winner])
+  @loser = Player.find_by_name(params[:loser])
+  debugger
+  @game = Game.last.update_attributes(race_time: params[:raceTime], winner: @winner.id, loser: @loser.id)
+  @ranking = Game.where(winner: @winner.id).order("race_time").limit(10)
+  redirect to '/victory'
+end
+
+
---
+++ public/js/racer.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+  raceStart = new Date();
+  console.log(raceStart);
+
+
+  $(this).keyup(function(event){
+    if (event.keyCode == 81){
+
+      $("#player1 td.active").removeClass('active').next().addClass('active');
+    }
+    else if (event.keyCode == 80){
+      $("#player2 td.active").removeClass('active').next().addClass('active');
+    }
+    if ( $('tr td:last-child').hasClass('active') )
+    {   
+      raceFinish = new Date();
+      raceTime = Math.abs(raceFinish - raceStart);
+      $(this).unbind('keyup');
+      var p1 = $("#player1name").text();
+      var p2 = $("#player2name").text();
+      if ($('tr:first-child td:last-child' ).hasClass('active'))
+        { 
+          alert(p1 + ' WINS!! In '  + raceTime / 1000.0 + ' seconds!');
+          var data = {
+            raceTime : raceTime,
+            winner : p1,
+            loser : p2
+          };
+
+          var params = $.param(data)
+
+          // console.log($params)
+          $.post('/victory', data, function(response)
+            {
+             $('body').html(response);
+            });
+        }
+      else
+        {
+          alert(p2 + ' WINS!! In ' +  raceTime / 1000.0 + ' seconds!');
+          var data = {
+            raceTime : raceTime,
+            winner : p2,
+            loser  : p1
+          };
+
+          var params = $.param(data)
+          $.post('/victory', data, function(response)
+          {
+            $('body').html(response)          
+          });
+        }
+    }
+  });
+});
